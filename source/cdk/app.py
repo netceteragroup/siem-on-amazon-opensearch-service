0ef9d27dfbc9861b8b728782f9ace19a87ff29d8
Pavel Nakonechnyi <pavel.nakonechnyi@netcetera.com>
2023-11-22 13:48:43 +0100

    WARN: source/cdk/app.py: adjusting stack definition
    In order to have smooth migration from old deployment model, we have to
    follow the previously chosen naming conventions. It means having Application
    named "DeploySiemStageProd-CNNAesSiemStack" and CloudFormation stack named
    "DeploySiemStageProd-CNNAesSiemStack" wich manages resources with hierarchy:
    "DeploySiemStageProd --> CNNAesSiemStack".

--- source/cdk/app.py
+++ source/cdk/app.py
@@ -14,6 +14,19 @@ import aws_cdk as cdk
 
 from mysiem.aes_siem_stack import MyAesSiemStack
 
+from constructs import Construct
+
+
+class SiemPipelineStack(cdk.Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+
+class SiemStage(cdk.Stage):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+
 app = cdk.App()
 CDK_DEFAULT_REGION = os.getenv(
     "CDK_DEFAULT_REGION", os.environ["AWS_DEFAULT_REGION"])
@@ -22,7 +35,26 @@ account = os.environ.get(
     "CDK_DEPLOY_ACCOUNT", os.environ["CDK_DEFAULT_ACCOUNT"])
 add_tags = os.environ.get("SIEM_ADD_TAGS", "")
 
-MyAesSiemStack(app, "aes-siem",
+# In order to have smooth migration from old deployment model, we have to
+# follow the previously chosen naming conventions. It means having Application
+# named "DeploySiemStageProd-CNNAesSiemStack" and CloudFormation stack named
+# "DeploySiemStageProd-CNNAesSiemStack" wich manages resources with hierarchy:
+# "DeploySiemStageProd --> CNNAesSiemStack".
+
+# The upstream class which defines the stack. For regular deployments,
+# uncomment it and delete additional classes defined above.
+
+# MyAesSiemStack(app, "aes-siem",
+#                description=f'SIEM on Amazon OpenSearch Service v{__version__}',
+#                env=cdk.Environment(account=account, region=region))
+
+siempipe = SiemPipelineStack(app, "SiemPipelineStack",
+                             env=cdk.Environment(account=account, region=region))
+
+siemstage = SiemStage(siempipe, "DeploySiemStageProd",
+                      env=cdk.Environment(account=account, region=region))
+
+MyAesSiemStack(siemstage, "CNNAesSiemStack",
                description=f'SIEM on Amazon OpenSearch Service v{__version__}',
                env=cdk.Environment(account=account, region=region))
 
